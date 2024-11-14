e42a9468f1a482955ab0ab1ddc6616d761bdf1c2
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-14 22:03:48 +0300

    Task7

---
+++ Tyuiu.KushnirS.Sprint4.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KushnirS.Sprint4.Task7.V5.Lib;
+namespace Tyuiu.KushnirS.Sprint4.Task7.V5.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint4.Task7.V5/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KushnirS.Sprint4.Task7.V5.Lib;
+namespace Tyuiu.KushnirS.Sprint4.Task7.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string v = "695324951753684";
+            int n = 5, m = 3;
+           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр.                                      *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 5 на 3 и подсчитайте сумму нечетных чисел.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 695324951753684                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Произведение четных элементов массива равно " + ds.Calculate(n, m, v));
+            Console.ReadLine();
+        }
+    }
+}

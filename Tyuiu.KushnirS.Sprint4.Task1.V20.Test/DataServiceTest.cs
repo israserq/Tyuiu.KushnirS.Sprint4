7ce37c9187390c4dc4fdd2a06536ec4acf18461c
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-14 20:46:54 +0300

    Task1

---
+++ Tyuiu.KushnirS.Sprint4.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KushnirS.Sprint4.Task1.V20.Lib;
+namespace Tyuiu.KushnirS.Sprint4.Task1.V20.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
